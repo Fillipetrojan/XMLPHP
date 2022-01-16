66cb955926e186b60b717ce7060bb2ae10443235
Fillipetrojan <43590987+Fillipetrojan@users.noreply.github.com>
2022-01-16 20:09:35 -0300

    estrutura e atributos
    Melhora na criação da estrutura.
    
    Cria Atributos para os nós.

--- class/XMLPHP.php
+++ class/XMLPHP.php
@@ -12,6 +12,8 @@ class XML
 
 
 
+
+
 	function __construct()
 	{
 		$this->XML =new DOMDocument('1.0', 'UTF-8');
@@ -21,6 +23,260 @@ class XML
 		$this->XML->formatOutput=true;
 
 	}
+
+
+	public function carregar_XML_file()
+	{
+
+		$this->XML->load('../XMLs/arquivo.xml');
+
+		$this->preserveWhiteSpace = true;
+
+		$this->XML->formatOutput = true;
+				
+		$this->XML->saveXML();
+
+	}/// fim carregar_XML_file()
+
+
+	//===========================================
+	//==================ESTRUTURA================
+	//===========================================
+
+
+
+		//=============================
+		// CRIAR
+
+			public function create_XML($name)
+			{
+
+				if($this->namexml!=null)
+				{
+
+					$this->namexml=$this->XML->createElement($name, " ");
+
+					$this->XML->appendChild($this->namexml);
+
+
+					$this->XML->save("../XMLs/arquivo.xml");
+
+
+					$XML_string=file_get_contents("../XMLs/arquivo.xml");
+
+					# $XML_string=str_replace("> <", ">\n<", $XML_string);
+
+					$arquivo=fopen("../XMLs/arquivo.xml", "wb");
+
+					fwrite($arquivo, $XML_string);
+
+
+					$this->carregar_XML_file();
+			
+				}else /// if($this->namexml!=null)
+				{
+					unset($this->XML);
+
+					$this->XML =new DOMDocument('1.0', 'UTF-8');
+
+					$this->namexml=$this->XML->createElement($name, " ");
+
+					$this->XML->appendChild($this->namexml);
+
+					$this->XML->save("../XMLs/arquivo.xml");
+
+
+					$XML_string=file_get_contents("../XMLs/arquivo.xml");
+
+					#$XML_string=str_replace("> <", ">\n<", $XML_string);
+
+					$arquivo=fopen("../XMLs/arquivo.xml", "wb");
+
+					fwrite($arquivo, $XML_string);
+
+
+					$this->carregar_XML_file();
+
+				} /// fim else if($this->namexml!=null)
+				
+			}/// fim public function create_XML($name)
+
+
+			
+			public function adicionar_elemento($name, $Pai)
+			{
+
+				$this->XML->save("../XMLs/arquivo.xml");
+
+
+				$XML_string=file_get_contents("../XMLs/arquivo.xml");
+
+
+				$pai_l=strlen($Pai);
+
+
+				$inicio=strpos($XML_string, "<$Pai>");
+
+				$fim=strpos($XML_string, "</$Pai>");
+
+
+				#$inicio+=$pai_l+2;
+
+				$texto_validacao=substr($XML_string, $inicio, $fim);
+
+
+				$validacao=strpos($texto_validacao, "<$name>");
+
+				
+				$validar_pai=strpos($texto_validacao, "<$Pai>");
+
+
+				///////////
+
+				$inicio=strpos($texto_validacao, "<$Pai>");
+
+				$fim=strpos($texto_validacao, "</$Pai>");
+
+
+				#$inicio+=$pai_l+2;
+
+				$texto_validacao=substr($texto_validacao, $inicio, $fim);
+
+
+				$validacao=strpos($texto_validacao, "<$name>");
+
+				
+				$validar_pai=strpos($texto_validacao, "<$Pai>");
+
+
+				if($validar_pai===false)
+				{
+					echo "<br>Elemento Pai não existe<br>";
+
+					return null;
+
+				} /// fim else if($validar_pai===false)
+
+				if($validacao===false)
+				{
+					$XML_string=str_replace("<$Pai>", "<$Pai> \n\t<$name></$name>", $XML_string);
+
+					$XML_string=str_replace("><", "> <", $XML_string);
+
+					
+
+					$XML_string=str_replace("$name> </$name", "$name> </$name", $XML_string);
+
+					$XML_string=str_replace("></$Pai", "> </$Pai", $XML_string);
+			
+					$arquivo=fopen("../XMLs/arquivo.xml", "wb");
+
+					fwrite($arquivo, $XML_string);
+
+
+					$this->carregar_XML_file();
+						
+
+					$this->XML->saveXML();
+				}else /// fim if($validação===false)
+				{
+					echo "<br>Elemento já existe";
+
+					return null;
+				}
+
+
+			} /// fim public function adicionar_elemento($name, $pai)
+
+			
+
+
+			public function adicionar_texto($name, $texto)
+			{
+
+				$XML_string=file_get_contents("../XMLs/arquivo.xml");
+
+				$arquivo=fopen("../XMLs/arquivo.xml", "wb");
+
+
+				$XML_string=str_replace("<$name>", "<$name> $texto. \t", $XML_string);
+
+				fwrite($arquivo, $XML_string);
+
+				$this->carregar_XML_file();
+
+			}/// fim public function adicionar_texto()
+
+
+			public function adicionar_atributo($name, $node, $valor)
+			{
+
+				$this->XML->save("../XMLs/arquivo.xml");
+
+				$XML_string=file_get_contents("../XMLs/arquivo.xml");
+
+				$arquivo=fopen("../XMLs/arquivo.xml", "wb");
+
+
+				$XML_string=str_replace("<$node", "<$node $name='$valor'", $XML_string);
+
+				fwrite($arquivo, $XML_string);
+
+				$this->carregar_XML_file();
+
+			} /// fim public function adicionar_atributo($name, $node, $valor)
+
+
+
+
+		//=============================
+		//=============================
+
+		
+
+
+
+		
+
+
+
+		
+
+	//===========================================
+	//===========================================
+	//===========================================
+
+
+
+
+	//===========================================
+	//==================VALORES==================
+	//===========================================
+
+
+
+
+
+		public function alterar_texto($name, $texto)
+		{
+
+		}/// fim public function altetar_texto($name, $texto)
+
+
+
+		public function alterar_atributo($name, $node, $valor)
+		{
+
+		}
+
+
+	//===========================================
+	//===========================================
+	//===========================================
+
+
+
+
 
 
 	private function formatar_DOM()
@@ -43,152 +299,8 @@ class XML
 	#$domtree = new DOMDocument('1.0', 'UTF-8');
 
 
-	public function create_XML($name)
-	{
-
-
-		if($this->namexml!=null)
-		{
-
-			
-
-			$this->namexml=$this->XML->createElement($name, " ");
-
-			$this->XML->appendChild($this->namexml);
-
-
-			$this->XML->save("../XMLs/arquivo.xml");
-
-
-			$XML_string=file_get_contents("../XMLs/arquivo.xml");
-
-			# $XML_string=str_replace("> <", ">\n<", $XML_string);
-
-			$arquivo=fopen("../XMLs/arquivo.xml", "wb");
-
-			fwrite($arquivo, $XML_string);
-
-
-			$this->XML->load('../XMLs/arquivo.xml');
-
-			$this->preserveWhiteSpace = true;
-
-			$this->XML->formatOutput = true;
-
-			#$this->XML->saveXML();
-
-			#echo $this->XML->saveXML();
-
-
-			
-		}else
-		{
-			unset($this->XML);
-
-			$this->XML =new DOMDocument('1.0', 'UTF-8');
-
-			$this->namexml=$this->XML->createElement($name, " ");
-
-			$this->XML->appendChild($this->namexml);
-
-			$this->XML->save("../XMLs/arquivo.xml");
-
-
-			$XML_string=file_get_contents("../XMLs/arquivo.xml");
-
-			#$XML_string=str_replace("> <", ">\n<", $XML_string);
-
-			$arquivo=fopen("../XMLs/arquivo.xml", "wb");
-
-			fwrite($arquivo, $XML_string);
-
-
-			$this->XML->load('../XMLs/arquivo.xml');
-
-			$this->preserveWhiteSpace = true;
-
-			$this->XML->formatOutput = true;
-
-			#$this->XML->saveXML();
-
-			#echo $this->XML->saveXML();
-
-			
-
-		}
-		
-	}/// fim public function create_XML($name)
-
-
-
-	public function adicionar_elemento($name, $Pai)
-	{
-
-		$this->XML->save("../XMLs/arquivo.xml");
-
-
-		$XML_string=file_get_contents("../XMLs/arquivo.xml");
-
-
-		$validacao=strpos($XML_string, "<$name>");
-
-
-
-		$validar_pai=strpos($XML_string, "<$Pai>");
-
-
-		if($validar_pai===false)
-		{
-			echo "<br>Elemento Pai não existe<br>";
-
-			return null;
-			
-		} /// fim else if($validar_pai===false)
-
-		if($validacao===false)
-		{
-			$XML_string=str_replace("<$Pai>", "<$Pai>\n\t<$name></$name>", $XML_string);
-
-			$XML_string=str_replace("><", "> <", $XML_string);
-
-			
-
-			$XML_string=str_replace("$name> </$name", "$name> </$name", $XML_string);
-
-			$XML_string=str_replace("></$Pai", "> </$Pai", $XML_string);
-
-
-
-			
-
-			
 	
-			$arquivo=fopen("../XMLs/arquivo.xml", "wb");
-
-			fwrite($arquivo, $XML_string);
-
-
-			$this->XML->load('../XMLs/arquivo.xml');
-
-			$this->preserveWhiteSpace = true;
-
-			$this->XML->formatOutput = true;
-				
-
-			#echo $this->XML->saveXML();
-		}else /// fim if($validação===false)
-		{
-			echo "<br>Elemento já existe";
-
-			return null;
-		} 
-
-			
-
-			#echo $XML_string;
-
-	} /// fim public function adicionar_elemento($name, $pai)
-
+	
 
 
 	public function delete_XML()
@@ -196,17 +308,13 @@ class XML
 		unset($this->XML);
 	}
 
-
-
 	public function return_XML()
 	{
 		return $this->XML->saveXML();
 		
 	}
 
-
-
-}# fim class XML
+}/// fim class XML
 
 
 ?>
--- exemple/test.php
+++ exemple/test.php
@@ -6,6 +6,8 @@ header("Content-Type: text/xml");
 
 $teste= new XML();
 
+
+
 $teste->delete_XML();
 
 $teste->create_XML("Venda");
@@ -18,16 +20,50 @@ $teste->adicionar_elemento("Produto", "Venda");
 
 $teste->adicionar_elemento("Servico", "Venda");
 
+
+
 $teste->adicionar_elemento("Laranja", "Produto");
+
+$teste->adicionar_elemento("Maçã", "Produto");
+
+$teste->adicionar_elemento("Fubá", "Produto");
+
+
+
+$teste->adicionar_elemento("Valor", "Laranja");
+
+$teste->adicionar_elemento("Valor", "Maçã");
+
+$teste->adicionar_elemento("Valor", "Fubá");
+
+
 
 $teste->adicionar_elemento("Cozinhar", "Servico");
 
-$xml_texto=$teste->return_XML();
+$teste->adicionar_texto("Cozinhar", "File com fritas");
 
+
+$valor_total=0;
+
+$teste->adicionar_atributo("Valor", "Maçã", "3.45");
+
+$valor_total+=3.45;
+
+$teste->adicionar_atributo("Valor", "Fubá", "12.78");
+
+$valor_total+=12.78;
+
+$teste->adicionar_atributo("ValorTotal", "Produto", $valor_total);
+
+$teste->adicionar_atributo("Venda_Total", "Venda", $valor_total);
+
+
+
+$xml_texto=$teste->return_XML();
 
 echo $xml_texto;
 
-
 $teste->delete_XML();
+
 
 ?>
